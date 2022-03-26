f2b3ab8d84cdc220b8babebdff6ce3fab5478078
Sophie Student <83235604+SophieStudent@users.noreply.github.com>
2022-03-26 20:56:15 +0000

    Updated salary program

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Ms Raffin
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict'
+
+// declare constants
+const TAX_BRACKET = 0.1805
+
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function payClicked () {
+  // get user input
+  let hours = parseFloat(document.getElementById('hours').value)
+  let rate = parseFloat(document.getElementById('rate').value)
+
+  // calculate the pay and the tax
+  let total_pay = hours * rate
+  let tax = TAX_BRACKET * total_pay
+	let total_earned = total_pay - tax 
+
+  // display the results
+  document.getElementById('total-earned').innerHTML = "Your pay will be $" + total_earned.toFixed(2)
+  document.getElementById('tax').innerHTML = "The government will take $" + tax.toFixed(2)
+}
